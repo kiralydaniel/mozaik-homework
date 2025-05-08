d608743d8a626c3127a676d7275d5db932e1e023
kiralydaniel <kiralydani93@gmail.com>
2025-05-08 11:07:18 +0200

    add function to get subcategory path

--- pages/home_page.py
+++ pages/home_page.py
@@ -9,6 +9,7 @@ class HomePage(BasePage):
         self.currency_dropdown = page.locator("a").filter(has_text="$ US Dollar").first
         self.currency_gbp = page.get_by_role("link", name="£ Pound Sterling")
         self.profile_menu = page.get_by_role("link", name="Welcome back")
+        self.subcategory_links_locator = self.page.locator("div.subcategories a")
 
     def user_is_logged_in(self):
         expect(self.profile_menu).to_be_visible()
@@ -22,3 +23,17 @@ class HomePage(BasePage):
         expect(self.currency_gbp).to_be_visible()
         self.currency_gbp.click()
 
+    def get_subcategory_links(self) -> list[dict]:
+        subcategory_links = self.subcategory_links_locator
+        count = subcategory_links.count()
+
+        subcategories = []
+        for i in range(count):
+            element = subcategory_links.nth(i)
+            name = element.inner_text().strip()
+            href = element.get_attribute("href")
+            if href and href.startswith(self.base_url):
+                path = href.replace(self.base_url, "")
+                subcategories.append({"name": name, "path": path})
+        
+        return subcategories
