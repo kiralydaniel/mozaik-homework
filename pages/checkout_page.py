4191163fdc7df49a6868efd4de15c4fee488c01f
kiralydaniel <kiralydani93@gmail.com>
2025-05-10 13:52:48 +0200

    add return types and shipping fee retrieval to CheckoutPage

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -6,14 +6,21 @@ class CheckoutPage(BasePage):
         super().__init__(page)
         self.confirm_order_button = page.get_by_role("button", name="Confirm Order")
         self.order_success_message = page.get_by_text("Your Order Has Been Processed!")
+        self.shipping_text = page.locator("text=Flat Shipping Rate:").locator("xpath=../following-sibling::td/span[contains(@class, 'bold')]")
 
-    def confirm_order(self):
+
+    def confirm_order(self) -> None:
         self.confirm_order_button.click()
 
-    def is_order_successful(self):
+    def is_order_successful(self) -> None:
         expect(self.order_success_message).to_be_visible()
     
-    def extract_order_number(self):
+    def extract_order_number(self) -> str:
         confirmation_text = self.page.get_by_text("Your order #").text_content().strip()
         order_number = confirmation_text.split("#")[1].split()[0]
         return order_number
+    
+    def get_shipping_fee(self) -> float:
+        expect(self.shipping_text).to_be_visible()
+        shipping_fee = float(self.shipping_text.inner_text().replace("£", "").strip())
+        return shipping_fee
